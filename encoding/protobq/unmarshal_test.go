cfd039b305844e295fa5c5be162d37bf5c27c5e5
Oscar Söderlund <odsod@users.noreply.github.com>
2021-01-23 17:31:09 +0100

    feat: implement unmarshal/load for wrappers
    Follow-up to implementation of marshaling.

--- encoding/protobq/unmarshal_test.go
+++ encoding/protobq/unmarshal_test.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/genproto/googleapis/example/library/v1"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 	"gotest.tools/v3/assert"
 )
 
@@ -127,6 +128,32 @@ func TestUnmarshalOptions_Unmarshal(t *testing.T) {
 			},
 			expected: &examplev1.ExampleEnum{
 				EnumValue: examplev1.ExampleEnum_ENUM_VALUE1,
+			},
+		},
+
+		{
+			name: "wrappers",
+			row: map[string]bigquery.Value{
+				"float_value":  float64(1),
+				"double_value": float64(2),
+				"string_value": "foo",
+				"bytes_value":  []byte("bar"),
+				"int32_value":  int64(3),
+				"int64_value":  int64(4),
+				"uint32_value": uint64(5),
+				"uint64_value": uint64(6),
+				"bool_value":   true,
+			},
+			expected: &examplev1.ExampleWrappers{
+				FloatValue:  wrapperspb.Float(1),
+				DoubleValue: wrapperspb.Double(2),
+				StringValue: wrapperspb.String("foo"),
+				BytesValue:  wrapperspb.Bytes([]byte("bar")),
+				Int32Value:  wrapperspb.Int32(3),
+				Int64Value:  wrapperspb.Int64(4),
+				Uint32Value: wrapperspb.UInt32(5),
+				Uint64Value: wrapperspb.UInt64(6),
+				BoolValue:   wrapperspb.Bool(true),
 			},
 		},
 	} {
