adf1524ca5a808176cb3b1bc1381534cbc691732
Alfred Gunnargård <alfred.gunnargard@einride.tech>
2021-11-05 15:48:13 +0100

    feat: add protoc plugin for generating json BigQuery schemas

---
+++ protoc-gen-bq-json-schema/genjson/message.go
@@ -0,0 +1,24 @@
+package genjson
+
+import (
+	"fmt"
+
+	"go.einride.tech/protobuf-bigquery/encoding/jsonbq"
+	"go.einride.tech/protobuf-bigquery/encoding/protobq"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func GenerateSchemaFile(gen *protogen.Plugin, msg *protogen.Message, config Config) error {
+	g := gen.NewGeneratedFile(fmt.Sprintf("%s.json", msg.Desc.Name()), "")
+	schema := protobq.SchemaOptions{}.InferMessageSchema(msg.Desc)
+	jsonOpt := jsonbq.MarshalOptions{}
+	if config.Format {
+		jsonOpt.Indent = "  "
+	}
+	out, err := jsonOpt.MarshalSchema(schema)
+	if err != nil {
+		return err
+	}
+	g.P(string(out))
+	return nil
+}
