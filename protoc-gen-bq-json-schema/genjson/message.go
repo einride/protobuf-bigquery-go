a9e4bf5a9c892d178b22f9b907d8aa358dbee5ba
Alfred Gunnargård <alfred.gunnargard@einride.tech>
2021-11-16 09:30:36 +0100

    feat(protoc-gen-bq-json-schema): add flags for SchemaOptions

--- protoc-gen-bq-json-schema/genjson/message.go
+++ protoc-gen-bq-json-schema/genjson/message.go
@@ -4,15 +4,12 @@ import (
 	"fmt"
 
 	"go.einride.tech/protobuf-bigquery/encoding/jsonbq"
-	"go.einride.tech/protobuf-bigquery/encoding/protobq"
 	"google.golang.org/protobuf/compiler/protogen"
 )
 
 func GenerateSchemaFile(gen *protogen.Plugin, msg *protogen.Message, config Config) error {
 	g := gen.NewGeneratedFile(fmt.Sprintf("%s.json", msg.Desc.Name()), "")
-	schema := protobq.SchemaOptions{
-		UseModeFromFieldBehavior: config.UseModeFromFieldBehavior,
-	}.InferMessageSchema(msg.Desc)
+	schema := config.SchemaOptions.InferMessageSchema(msg.Desc)
 	jsonOpt := jsonbq.MarshalOptions{}
 	if config.Format {
 		jsonOpt.Indent = "  "
