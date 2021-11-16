948ff27157f318ba678128f12cb2789316630175
Alfred Gunnargård <alfred.gunnargard@einride.tech>
2021-11-16 09:30:36 +0100

    feat(protoc-gen-bq-json-schema): add flags for SchemaOptions

--- protoc-gen-bq-json-schema/genjson/config.go
+++ protoc-gen-bq-json-schema/genjson/config.go
@@ -1,10 +1,13 @@
 package genjson
 
-import "github.com/spf13/pflag"
+import (
+	"github.com/spf13/pflag"
+	"go.einride.tech/protobuf-bigquery/encoding/protobq"
+)
 
 type Config struct {
-	Format                   bool
-	UseModeFromFieldBehavior bool
+	Format        bool
+	SchemaOptions protobq.SchemaOptions
 }
 
 func (c *Config) AddToFlagSet(flags *pflag.FlagSet) {
@@ -15,9 +18,28 @@ func (c *Config) AddToFlagSet(flags *pflag.FlagSet) {
 		"Set to true to get a formatted json output.",
 	)
 	flags.BoolVar(
-		&c.UseModeFromFieldBehavior,
-		"field_behavior_mode",
+		&c.SchemaOptions.UseEnumNumbers,
+		"enum_numbers",
 		false,
-		"Set to true to use google api field behavior for setting field mode.",
+		"Map proto enum fields to BQ INTEGER fields.",
+	)
+	flags.BoolVar(
+		&c.SchemaOptions.UseDateTimeWithoutOffset,
+		"datetime_without_offset",
+		false,
+		"Convert google.type.DateTime fields to DATETIME and discard the optional time offset.",
+	)
+	flags.BoolVar(
+		&c.SchemaOptions.UseOneofFields,
+		"oneof_fields",
+		false,
+		"Add an extra STRING field for oneof fields with the name of the oneof, "+
+			"to contain the name of the field that is set.",
+	)
+	flags.BoolVar(
+		&c.SchemaOptions.UseModeFromFieldBehavior,
+		"mode_from_field_behavior",
+		false,
+		"Set the mode of a field to REQUIRED if the field is defined with REQUIRED behavior in proto.",
 	)
 }
