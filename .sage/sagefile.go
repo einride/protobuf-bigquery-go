c8ce6d41d53f3e66c58332fe010843438f20baae
Oscar Söderlund <odsod@users.noreply.github.com>
2022-04-03 17:28:43 +0200

    ci: migrate to Sage

---
+++ .sage/sagefile.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/tools/sgconvco"
+	"go.einride.tech/sage/tools/sggit"
+	"go.einride.tech/sage/tools/sggo"
+	"go.einride.tech/sage/tools/sggolangcilint"
+	"go.einride.tech/sage/tools/sggoreview"
+	"go.einride.tech/sage/tools/sgmarkdownfmt"
+	"go.einride.tech/sage/tools/sgyamlfmt"
+)
+
+func main() {
+	sg.GenerateMakefiles(
+		sg.Makefile{
+			Path:          sg.FromGitRoot("Makefile"),
+			DefaultTarget: All,
+		},
+		sg.Makefile{
+			Path:          sg.FromGitRoot("internal", "examples", "proto", "Makefile"),
+			Namespace:     Proto{},
+			DefaultTarget: Proto.All,
+		},
+	)
+}
+
+func All(ctx context.Context) error {
+	sg.Deps(ctx, ConvcoCheck, FormatMarkdown, FormatYAML, Proto.All)
+	sg.Deps(ctx, GoLint, GoReview, GoTest)
+	sg.SerialDeps(ctx, GoModTidy, GitVerifyNoDiff)
+	return nil
+}
+
+func FormatYAML(ctx context.Context) error {
+	sg.Logger(ctx).Println("formatting YAML files...")
+	return sgyamlfmt.Command(ctx, "-d", sg.FromGitRoot(), "-r").Run()
+}
+
+func GoModTidy(ctx context.Context) error {
+	sg.Logger(ctx).Println("tidying Go module files...")
+	return sg.Command(ctx, "go", "mod", "tidy", "-v").Run()
+}
+
+func GoTest(ctx context.Context) error {
+	sg.Logger(ctx).Println("running Go tests...")
+	return sggo.TestCommand(ctx).Run()
+}
+
+func GoReview(ctx context.Context) error {
+	sg.Logger(ctx).Println("reviewing Go files...")
+	return sggoreview.Command(ctx, "-c", "1", "./...").Run()
+}
+
+func GoLint(ctx context.Context) error {
+	sg.Logger(ctx).Println("linting Go files...")
+	return sggolangcilint.Run(ctx)
+}
+
+func FormatMarkdown(ctx context.Context) error {
+	sg.Logger(ctx).Println("formatting Markdown files...")
+	return sgmarkdownfmt.Command(ctx, "-w", ".").Run()
+}
+
+func ConvcoCheck(ctx context.Context) error {
+	sg.Logger(ctx).Println("checking git commits...")
+	return sgconvco.Command(ctx, "check", "origin/master..HEAD").Run()
+}
+
+func GitVerifyNoDiff(ctx context.Context) error {
+	sg.Logger(ctx).Println("verifying that git has no diff...")
+	return sggit.VerifyNoDiff(ctx)
+}
