e08fcab05b60a1d3028d3622f077877017018b70
Niklas Kullberg <niklas.kullberg@einride.tech>
2022-09-12 17:01:33 +0200

    feat: use full proto package name in generated schema filenames

--- protoc-gen-bq-json-schema/genjson/message.go
+++ protoc-gen-bq-json-schema/genjson/message.go
@@ -2,13 +2,15 @@ package genjson
 
 import (
 	"fmt"
+	"strings"
 
 	"go.einride.tech/protobuf-bigquery/encoding/jsonbq"
 	"google.golang.org/protobuf/compiler/protogen"
 )
 
 func GenerateSchemaFile(gen *protogen.Plugin, msg *protogen.Message, config Config) error {
-	g := gen.NewGeneratedFile(fmt.Sprintf("%s.json", msg.Desc.Name()), "")
+	filename := strings.ReplaceAll(strings.ToLower(string(msg.Desc.FullName())), ".", "_")
+	g := gen.NewGeneratedFile(fmt.Sprintf("%s.json", filename), "")
 	schema := config.SchemaOptions.InferMessageSchema(msg.Desc)
 	jsonOpt := jsonbq.MarshalOptions{}
 	if config.Format {
