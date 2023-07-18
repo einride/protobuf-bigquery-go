cff89f6600edca499b5b58905bd9a507ab7c8766
Jusabe Guedes <jusabe.guedes@einride.tech>
2023-07-18 11:08:56 +0200

    feat: update sage

--- .sage/sagefile.go
+++ .sage/sagefile.go
@@ -36,7 +36,7 @@ func All(ctx context.Context) error {
 
 func FormatYAML(ctx context.Context) error {
 	sg.Logger(ctx).Println("formatting YAML files...")
-	return sgyamlfmt.Command(ctx, "-d", sg.FromGitRoot(), "-r").Run()
+	return sgyamlfmt.Run(ctx)
 }
 
 func GoModTidy(ctx context.Context) error {
