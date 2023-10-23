565ef672b150a90eba3b50b25514e1238be9eafa
Fuyang Liu <liufuyang@users.noreply.github.com>
2023-10-23 12:46:37 +0200

    chore(test): add test to check zero value behaviour for MessageSaver

--- encoding/protobq/messagesaver_test.go
+++ encoding/protobq/messagesaver_test.go
@@ -29,3 +29,21 @@ func TestMessageSaver_Save(t *testing.T) {
 	}
 	assert.DeepEqual(t, expected, actual)
 }
+
+func TestMessageSaver_Save_with_zero_value(t *testing.T) {
+	messageSaver := MessageSaver{
+		Message: &library.ListShelvesRequest{
+			PageSize:  0,
+			PageToken: "token",
+		},
+		InsertID: "test-insert-id",
+	}
+	actual, insertID, err := messageSaver.Save()
+	assert.NilError(t, err)
+	assert.Equal(t, messageSaver.InsertID, insertID)
+	expected := map[string]bigquery.Value{
+		"page_size":  int64(0), // fails as a proto zero value is not wired so the generated message does not contain it
+		"page_token": "token",
+	}
+	assert.DeepEqual(t, expected, actual)
+}
